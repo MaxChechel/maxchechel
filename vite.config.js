cc58f4bbf1915aee81fca980b01884e9b4d9fdb2
Max Chechel <maksymchechel@yahoo.com>
2023-06-05 18:29:57 +0200

    file structure and bundled js file name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
+  plugins: [eslintPlugin({ cache: false })],
   server: {
     port: 3000,
+  },
+  build: {
+    minify: true,
+    manifest: true,
+    entryFileNames: "index.js",
+    rollupOptions: {
+      input: "./src/main.js",
+    },
   },
 });
